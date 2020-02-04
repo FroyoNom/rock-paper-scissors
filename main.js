9ca85227ac40e5fa6c3ff78bc246fb0178cedd9f
Simon O <noneing656@protonmail.com>
2020-02-03 20:41:23 -0800

    implemented playRound function

--- main.js
+++ main.js
@@ -33,3 +33,53 @@ const playerSelection = () => {
     return "Wrong input, you must select either rock, paper or scissors.";
   }
 };
+
+const playRound = (playerChoice, computerChoice) => {
+  let playerScore = false;
+  switch (playerChoice) {
+    case "rock":
+      if (computerChoice === playerChoice) {
+        return "Rock! It's a tie!";
+      } else if (computerChoice === "paper") {
+        ("You Lose! Paper beats Rock!");
+        playerScore = false;
+
+        return playerScore;
+      } else {
+        ("You Win! Rock beats Scissors!");
+        playerScore = true;
+
+        return playerScore;
+      }
+
+    case "paper":
+      if (computerChoice === playerChoice) {
+        return "Paper! It's a tie!";
+      } else if (computerChoice === "Paper") {
+        ("You Lose! Scissors beats Paper!");
+        playerScore = false;
+
+        return playerScore;
+      } else {
+        ("You Win! Paper beats Scissors!");
+        playerScore = true;
+
+        return playerScore;
+      }
+
+    case "scissors":
+      if (computerChoice === playerChoice) {
+        return "Scissors! It's a tie!";
+      } else if (computerChoice === "Scissors") {
+        ("You Lose! Rock beats Scissors!");
+        playerScore = false;
+
+        return playerScore;
+      } else {
+        ("You Win! Scissors beats Scissors!");
+        playerScore = true;
+
+        return playerScore;
+      }
+  }
+};
